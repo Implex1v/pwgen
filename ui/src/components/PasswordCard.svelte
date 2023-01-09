c45c5ab87afdd4b3c81eb916f3d68eeccb625323
Implex1v <Implex1v@gmail.com>
2023-01-09 20:29:03 +0100

    (ui) Improve password history

--- ui/src/components/PasswordCard.svelte
+++ ui/src/components/PasswordCard.svelte
@@ -1,29 +1,80 @@
+<script lang="ts" context="module">
+    import type { PasswordModel } from "$lib/types";
+
+    export default interface PasswordDeleteEvent {
+        password: PasswordModel;
+    }
+</script>
+
 <script lang="ts">
     import { Icon } from "svelte-awesome";
-    import { eye, eyeSlash } from "svelte-awesome/icons";
+    import { check, clipboard, eye, eyeSlash, trash } from "svelte-awesome/icons";
+    import { createEventDispatcher } from "svelte";
 
-    export let password = "";
+    const dispatcher = createEventDispatcher();
+
+    export let password: PasswordModel;
     let showPassword = false;
+    let copied = false;
 
     const toggleVisibility = async () => {
         console.log("toggle");
         showPassword = !showPassword;
     };
+
+    const onCopy = async () => {
+        await navigator.clipboard.writeText(password.password);
+        copied = true;
+    };
+
+    const onDelete = async () => {
+        dispatcher("passwordDelete", {
+            password: password,
+        });
+    }
 </script>
 
-<div class="p-4 border-2 border-gray-700 rounded-lg my-2">
-    <p class="text-lg font-bold">Password</p>
-    <div class="pt-2">
-        {#if showPassword}
-            <button on:click={toggleVisibility} class="hover:text-amber-700">
+<div class="my-2 flex">
+    <div
+      on:click={toggleVisibility}
+      class="border rounded-l p-4 border-gray-500 hover:text-amber-700 cursor-pointer"
+    >
+        <button>
+            {#if showPassword}
                 <Icon data={eyeSlash} class="pb-1" scale={1.3} />
-            </button>
-            <span>{password}</span>
-        {:else}
-            <button on:click={toggleVisibility} class="hover:text-amber-700">
+            {:else}
                 <Icon data={eye} class="pb-1" scale={1.3} />
-            </button>
-            <span>****************</span>
+            {/if}
+        </button>
+    </div>
+    <div
+      on:click={onDelete}
+      class="border p-4 border-gray-500 hover:text-red-600 cursor-pointer"
+    >
+        <button>
+            <Icon data={trash} class="pb-1" scale={1.3} />
+        </button>
+    </div>
+    <div
+      on:click={onCopy}
+      class="border p-4 border-gray-500 hover:text-amber-700 cursor-pointer"
+    >
+        <button>
+            {#if copied}
+                <Icon data={check} class="pb-1 text-green-600" scale={1.3} />
+            {:else }
+                <Icon data={clipboard} class="pb-1" scale={1.3} />
+            {/if}
+        </button>
+    </div>
+    <div class="border p-4 border-gray-500">
+        <span>{password.generatedAt.toISOString()}</span>
+    </div>
+    <div class="border rounded-r p-4 border-gray-500 flex-auto">
+        {#if showPassword}
+            <span on:dblclick={onCopy}>{password.password}</span>
+        {:else}
+            <span>{"*".repeat(password.password.length)}</span>
         {/if}
     </div>
 </div>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,13 +2,24 @@
     import type PasswordEvent from "../components/PasswordGenerator.svelte";
     import PasswordCard from "../components/PasswordCard.svelte";
     import PasswordGenerator from "../components/PasswordGenerator.svelte";
+    import type { PasswordModel } from "$lib/types";
+    import type PasswordDeleteEvent from "../components/PasswordCard.svelte";
 
-    let passwords: Array<string> = [];
+    let passwords: Array<PasswordModel> = [];
 
     async function addPassword(event: CustomEvent<PasswordEvent>) {
-        const tmp = [event.detail.password];
+        const model: PasswordModel = {
+            password: event.detail.password,
+            generatedAt: new Date(),
+        }
+
+        const tmp = [model];
         tmp.push(...passwords);
         passwords = tmp;
+    }
+
+    async function deletePassword(event: CustomEvent<PasswordDeleteEvent>) {
+        passwords = passwords.filter((password) => { return password.password != event.detail.password.password });
     }
 </script>
 
@@ -21,5 +32,5 @@
 <p class="text-2xl py-4">History</p>
 
 {#each passwords as password}
-    <PasswordCard {password} />
+    <PasswordCard {password} on:passwordDelete={deletePassword} />
 {/each}
